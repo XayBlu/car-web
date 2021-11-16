5327ab399e90e8951320b87bd78ee4e09b77bee3
xavier <simms.xavier@gmail.com>
2021-11-16 19:34:05 +0000

    XS - Add Spring initializr skeleton project.

---
+++ src/test/java/com/carweb/rest/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carweb.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
