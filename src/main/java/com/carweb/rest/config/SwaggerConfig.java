8f68e90f1837e66d7f632417004d6e2bd26426c6
xavier <simms.xavier@gmail.com>
2021-11-19 21:54:00 +0000

    XS - Simplify swagger config.

--- src/main/java/com/carweb/rest/config/SwaggerConfig.java
+++ src/main/java/com/carweb/rest/config/SwaggerConfig.java
@@ -2,8 +2,7 @@ package com.carweb.rest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,10 +14,6 @@ public class SwaggerConfig {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/");
+                .tags(new Tag("Car Entity", "Repository for car entities"));
     }
 }
